270352b39be06ed1689369581867af32e8c6707c
trevrd22 <trevrd22@gmail.com>
2024-11-20 17:04:19 -0600

    suppressed biome

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
