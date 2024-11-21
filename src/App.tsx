8acd71a905778a02195e92dac5a073f7ecad0bde
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-11-20 16:08:17 -0800

    biome formatter

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import FPB from "./components/FPB";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div className="format">
-        <h1 style={{ marginBottom: "20px" }}>Trevor's Room Booking Form</h1>
-        <div>
-          <FPB />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="format">
+				<h1 style={{ marginBottom: "20px" }}>Trevor's Room Booking Form</h1>
+				<div>
+					<FPB />
+				</div>
+			</div>
+		</>
+	);
 }
 export default App;
--- src/components/FPB.tsx
+++ src/components/FPB.tsx
@@ -2,28 +2,28 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
 export default function FPB() {
-  return (
-    <Stack spacing={2} direction="column">
-      <>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            alert("WIP");
-          }}
-        >
-          Book A Room
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            alert("WIP");
-          }}
-        >
-          View Room Booking Statuses
-        </Button>
-      </>
-    </Stack>
-  );
+	return (
+		<Stack spacing={2} direction="column">
+			<>
+				<Button
+					variant="contained"
+					color="secondary"
+					onClick={() => {
+						alert("WIP");
+					}}
+				>
+					Book A Room
+				</Button>
+				<Button
+					variant="contained"
+					color="secondary"
+					onClick={() => {
+						alert("WIP");
+					}}
+				>
+					View Room Booking Statuses
+				</Button>
+			</>
+		</Stack>
+	);
 }
