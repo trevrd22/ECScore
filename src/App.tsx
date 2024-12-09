d46ef27228e3a53a4fda6ba382c84e6c757d3a2e
trevrd22 <trevrd22@gmail.com>
2024-12-09 15:16:28 -0600

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import FPB from "./components/FPB";
 import "./App.css";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <div className="format">
+    <Router>
+      <Routes>
+        <Route path="/">
+        <div className="format">
         <h1 style={{ marginBottom: "20px" }}>Trevor's Room Booking Form</h1>
         <div>
           <FPB />
         </div>
       </div>
-    </>
+      </Route>
+      </Routes>
+    </Router>
   );
 }
 export default App;
